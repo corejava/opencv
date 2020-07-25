491de94fa138a18a75b3ee7474b73b4c2cdf87a6
Jon Yao <yaoyunjiao@gmail.com>
2020-07-25 17:59:58 +0800

    清理结构，简化测试图片即可

--- samples/cpp/example_cmake/test/testImageStitching/main.cpp
+++ samples/cpp/example_cmake/test/testImageStitching/main.cpp
@@ -1,53 +1,97 @@
 #include <opencv2/opencv.hpp>
-#include <iostream>
+#include <opencv2/xfeatures2d.hpp>
 #include <string>
 
-using namespace cv;
 using namespace std;
+using namespace cv;
 
-int main(int argc, char **argv) {
-//    vector<string> files;
-//    glob("../images", files);
-    vector<Mat> images;
-//    for (int i = 0; i < files.size(); i++) {
-//        printf("image file : %s \n", files[i].c_str());
-//        images.push_back(imread(files[i]));
-//    }
-    string file_path = "../../images/";
-    images.push_back(imread(file_path + "1.png"));
-    images.push_back(imread(file_path + "2.png"));
-    // 设置拼接模式与参数
-    Mat result1, result2, result3;
-    Stitcher::Mode mode = Stitcher::PANORAMA;
-    Ptr<Stitcher> stitcher = Stitcher::create(mode);
+int main() {
 
-    // 拼接方式-多通道融合
-    auto blender = detail::Blender::createDefault(detail::Blender::MULTI_BAND);
-    stitcher->setBlender(blender);
+    string root_path = "~/fork/opencv/samples/cpp/example_cmake/";
+    string file_path = root_path + "images/";
 
-    // 拼接
-    Stitcher::Status status = stitcher->stitch(images, result1);
 
-    // 平面曲翘拼接
-    auto plane_warper = makePtr<cv::PlaneWarper>();
-    stitcher->setWarper(plane_warper);
-    status = stitcher->stitch(images, result2);
+    Mat imageL = imread(file_path + "1.jpg", 1);    //右图
+    Mat imageR = imread(file_path + "2.jpg", 1);    //左图
 
-    // 鱼眼拼接
-    auto fisheye_warper = makePtr<cv::FisheyeWarper>();
-    stitcher->setWarper(fisheye_warper);
-    status = stitcher->stitch(images, result3);
 
-    // 检查返回
-    if (status != Stitcher::OK) {
-        cout << "Can't stitch images, error code = " << int(status) << endl;
-        return EXIT_FAILURE;
+//    imshow("p2", imageL);
+//    imshow("p1", imageR);
+
+    //提取特征点方法
+    //SIFT
+    //cv::Ptr<cv::xfeatures2d::SIFT> sift = cv::xfeatures2d::SIFT::create();
+    //ORB
+    //cv::Ptr<cv::ORB> orb = cv::ORB::create();
+    //SURF
+    cv::Ptr<cv::xfeatures2d::SURF> surf = cv::xfeatures2d::SURF::create();
+
+    //特征点
+    std::vector<cv::KeyPoint> keyPointL, keyPointR;
+    //单独提取特征点
+    surf->detect(imageL, keyPointL);
+    surf->detect(imageR, keyPointR);
+
+    //画特征点
+    cv::Mat keyPointImageL;
+    cv::Mat keyPointImageR;
+    drawKeypoints(imageL, keyPointL, keyPointImageL, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+    drawKeypoints(imageR, keyPointR, keyPointImageR, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+
+    //显示窗口
+    cv::namedWindow("KeyPoints of imageL");
+    cv::namedWindow("KeyPoints of imageR");
+
+    //显示特征点
+    cv::imshow("KeyPoints of imageL", keyPointImageL);
+    cv::imshow("KeyPoints of imageR", keyPointImageR);
+
+    //特征点匹配
+    cv::Mat despL, despR;
+    //提取特征点并计算特征描述子
+    surf->detectAndCompute(imageL, cv::Mat(), keyPointL, despL);
+    surf->detectAndCompute(imageR, cv::Mat(), keyPointR, despR);
+
+    //Struct for DMatch: query descriptor index, train descriptor index, train image index and distance between descriptors.
+    //int queryIdx –>是测试图像的特征点描述符（descriptor）的下标，同时也是描述符对应特征点（keypoint)的下标。
+    //int trainIdx –> 是样本图像的特征点描述符的下标，同样也是相应的特征点的下标。
+    //int imgIdx –>当样本是多张图像的话有用。
+    //float distance –>代表这一对匹配的特征点描述符（本质是向量）的欧氏距离，数值越小也就说明两个特征点越相像。
+    std::vector<cv::DMatch> matches;
+
+    //如果采用flannBased方法 那么 desp通过orb的到的类型不同需要先转换类型
+    if (despL.type() != CV_32F || despR.type() != CV_32F) {
+        despL.convertTo(despL, CV_32F);
+        despR.convertTo(despR, CV_32F);
     }
-    file_path = "../../result/";
-    imwrite(file_path + "result1.png", result1);
-    imwrite(file_path + "result2.png", result2);
-    imwrite(file_path + "result3.png", result3);
 
-    waitKey(0);
+    cv::Ptr<cv::DescriptorMatcher> matcher = cv::DescriptorMatcher::create("FlannBased");
+    matcher->match(despL, despR, matches);
+
+    //计算特征点距离的最大值
+    double maxDist = 0;
+    for (int i = 0; i < despL.rows; i++) {
+        double dist = matches[i].distance;
+        if (dist > maxDist)
+            maxDist = dist;
+    }
+
+    //挑选好的匹配点
+    std::vector<cv::DMatch> good_matches;
+    for (int i = 0; i < despL.rows; i++) {
+        if (matches[i].distance < 0.5 * maxDist) {
+            good_matches.push_back(matches[i]);
+        }
+    }
+
+
+    cv::Mat imageOutput;
+    cv::drawMatches(imageL, keyPointL, imageR, keyPointR, good_matches, imageOutput);
+
+    cv::namedWindow("picture of matching");
+    cv::imwrite(root_path + "result/result.png", imageOutput);
+//    cv::imshow("picture of matching", imageOutput);
+    cv::waitKey(0);
     return 0;
 }
+
